db2f34b4fd402d674baf9f745f15c95a19050f4e
Bhuvanshwr <bhuvanshwr@gmail.com>
2020-12-28 22:07:39 +0530

    commit

---
+++ helloworld/src/main/java/HelloWorld.java
@@ -0,0 +1,11 @@
+
+import greeter.src.main.java.Greeter;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        Greeter greeter=new Greeter();
+        System.out.println(greeter.sayHello());
+    }
+}
